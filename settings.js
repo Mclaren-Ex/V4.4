ca799e107a3cb19f9f22756028285c918e531892
Bolaolat <bolaolat300@gmail.com>
2024-12-13 10:45:12 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2347041039367"
+global.pairingNumber = "2347042259025"
 
-global.botNumber = ["2347041039367"]
+global.botNumber = ["2347042259025"]
 
 global.anticall = false
 
@@ -28,9 +28,9 @@ global.sign = "〆"
 // ☞ ➻ ➽ ☛ 〆 
 global.bank = "Opay"
 
-global.bankname = "BLUE"
+global.bankname = "Richard Elijah"
 
-global.accnumber = "7041039367"
+global.accnumber = "7042259025"
 
 global.language = "en"
 global.sessionName = "session"
