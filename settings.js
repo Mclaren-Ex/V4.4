fdeb9824a719877552a1058b7bfb9676b169aff8
Bolaolat <bolaolat300@gmail.com>
2024-12-18 19:30:08 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2347041039367"
+global.pairingNumber = "2349019529423"
 
-global.botNumber = ["2347041039367"]
+global.botNumber = ["2349019529423"]
 
 global.anticall = false
 
